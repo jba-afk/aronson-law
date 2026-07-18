0d101583d110dd6fa0a0c43af8e4ac3c35dd4978
jba-afk <jba@aronsonlaw.com>
2026-07-18 13:18:37 -0400

    Add Mitch planning and practice focus sections

--- app/our-attorneys/mitchell-cornwell/page.tsx
+++ app/our-attorneys/mitchell-cornwell/page.tsx
@@ -1,5 +1,13 @@
 import MitchHero from "@/components/our-attorneys/mitchell-cornwell/MitchHero";
+import MitchStory from "@/components/our-attorneys/mitchell-cornwell/MitchStory";
+import MitchHighlights from "@/components/our-attorneys/mitchell-cornwell/MitchHighlights";
 
 export default function MitchellCornwellPage() {
-  return <MitchHero />;
+  return (
+    <>
+      <MitchHero />
+      <MitchStory />
+      <MitchHighlights />
+    </>
+  );
 }
